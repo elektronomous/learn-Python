701e855c0f2d13fe78781e32874b5aa386439a83
elektronomous <akbarfaza222@gmail.com>
2022-09-27 05:54:46 +0800

    create day02 learning file

---
+++ day02/main.py
@@ -0,0 +1,13 @@
+# Data Types
+
+# String
+print("Muhammad Faza Akbar"[0]);    # M
+myName = "Muhammad Faza Akbar";
+
+print(myName[0] + myName[4] + myName[5]); # Mmm
+print(myName[3] + myName[6] + myName[10] + myName[12] + myName[14] + myName[17]); # aaaaAa
+
+"""
+what happens when using "123" + "456"
+"""
+print("123" + "456");   # 123456
