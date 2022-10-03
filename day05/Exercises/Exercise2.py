a4ddaac500c837a27305b68effddab04f639333c
elektronomous <akbarfaza222@gmail.com>
2022-10-04 06:55:13 +0800

    doing exercise day05

---
+++ day05/Exercises/Exercise2.py
@@ -0,0 +1,9 @@
+"""
+You're going to write a program that calculates the highest score
+from a List of scores.
+
+student_scores = [78, 65, 89, 86, 55];
+
+Important: you are not allowed to use the max or min function.
+"""
+student_scores = [1,2,3,4];
