f3915ffff1c87438cf0e3e12796823e369e4bddd
elektronomous <akbarfaza222@gmail.com>
2022-10-03 17:33:08 +0800

    doing exercise

--- day03/main.py
+++ day03/main.py
@@ -1,3 +1,5 @@
+from emoji import emojize
+
 """
 say that you have a bathtub and the water when it reaches more than
 the 50 centimeter(for example) level of bathtub, the water will drain by
@@ -19,7 +21,38 @@ you have rules that the person that would play, should be height
 print("Welcome to the rollercoaster!");
 height = int(input("What is your height in cm? "));
 
-if height > 120:
-    print("you can ride the rollercoaster!");
+if height >= 120:
+    bill = 0;
+    print(f"You can riding the {emojize('🎢')}");    
+    """ 
+    now you need to code the price problem. When the person age is
+    less than 12 years old it should pay for 5$, less than 18, but
+    more than 12 it should pay 12$, if the person more than 18 then
+    it should pay for 15$ 
+    """
+    age = int(input("What's your age? "));
+    
+    if age < 12:
+        bill = 5;
+        print("Please pay 5$");
+    elif age <= 18:
+        bill = 12;
+        print("Please pay 12$");
+    else:
+        bill = 15;
+        print("Please pay 15$");
+    
+    """
+    you can offer the person the memory when they playing the 🎢
+    with charge only 3$.
+    """
+    wantsPhotos = input("Do you want a photo taken? Y or N.");
+    
+    if wantsPhotos == "Y":
+        bill += 3;
+    
+    print(f"Your bill is {bill}$");
+        
 else:
-    print("you can't ride the rollercoaster")
+    print(f"You can play the roller coaster {emojize('😔')}");
+
