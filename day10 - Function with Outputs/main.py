9f9bb1dc097acab4e9f39a81aa3d5da21e331279
Muhammad Faza Akbar <akbarfaza222@gmail.com>
2022-11-07 19:14:31 +0800

    day10 done

--- day10 - Function with Outputs/main.py
+++ day10 - Function with Outputs/main.py
@@ -20,7 +20,10 @@ this is function result an output, where it result from the return statement
 """
 
 """ SMALL CHALLENGE """
+# create a docstring  for this function
 def format_name(f_name:str, l_name:str):
+    """Take a first and last name and format it
+    to return the title case version of the name."""
     if not f_name or not l_name:
         return "You don't specify the first and the last name.";
 
